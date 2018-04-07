b4d98552bd012bd5d733d767bcef007294402372
Mick <figura.m@web.de>
2018-04-07 04:00:23 +0200

    map

--- core/mapdata.js
+++ core/mapdata.js
@@ -6,6 +6,18 @@
      constructor(chunks) {
          this.data = chunks;
      }
+
+     getChunk(x, y) {
+         return this.data[x + ":" + y];
+     }
+
+     setChunk(x, y) {
+         return this.data[x + ":" + y];
+     }
+
+     isChunkExisting(x, y) {
+         return this.getChunk(x, y) ? true : false;
+     }
  }
 
  module.exports = Map;
